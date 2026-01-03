5b41ef09d17f2c0863cdfbfd9faff8f50f232fe6
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2026-01-03 12:35:54 +0700

    fix: fixing path ingress and next js

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    basePath: "/portofolio/show/test-sharing-vision-nextjs",
+    assetPrefix: "/portofolio/show/test-sharing-vision-nextjs",
 };
 
 export default nextConfig;
