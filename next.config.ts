038de95d02a25700063d5aab922b6809133a9cd5
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2026-01-04 01:29:01 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
-    basePath: "/portofolio/show/test-sharing-vision-nextjs",
-    assetPrefix: "/portofolio/show/test-sharing-vision-nextjs",
+    basePath: "/portofolio/show/test-sharing-vision-nextjs", // For Deploy To Kubernetes
+    assetPrefix: "/portofolio/show/test-sharing-vision-nextjs", // For Deploy To Kubernetes
 };
 
 export default nextConfig;
